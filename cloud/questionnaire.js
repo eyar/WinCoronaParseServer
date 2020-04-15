aeab1602c64214bd2b1b9d06da70838edd6c0eea
Eyar <eyar.gilad@gmail.com>
2020-04-15 06:55:03 +0300

    one_time_heb

--- cloud/questionnaire.js
+++ cloud/questionnaire.js
@@ -1,6 +1,6 @@
 ['one_time_heb', 'daily'].forEach(type=>{
     Parse.Cloud.define(type, async req=>{
-        const query = new Parse.Query("Question");
+        const query = new Parse.Query("question");
         query.equalTo("questionnaireType", type);
         const results = await query.find({useMasterKey: true});
         const questions = results.map(obj=>{
